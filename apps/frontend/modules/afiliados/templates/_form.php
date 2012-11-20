74e98ece61cc1c61e3380a85ca5186b00b42e464
Nestor <nestor@HP-Pavilion-dv5-2133.(none)>
2012-11-20 18:10:53 -0300

    estilo abm afiliado

--- apps/frontend/modules/afiliados/templates/_form.php
+++ apps/frontend/modules/afiliados/templates/_form.php
@@ -11,57 +11,86 @@
         
 <?php echo $form->renderGlobalErrors() ?>
 <div id="afiliadoIzquierda">
-<?php echo $form['apenombre']->renderLabel("Nombre y Apellido:") ?>        
-<?php echo $form['apenombre']->renderError() ?>
-<?php echo $form['apenombre'] ?>
-<label>Documento:</label>
-<?php echo $form['tipodoc_id']->renderError() ?>
-<?php echo $form['tipodoc_id']->render(array('class'=>'tipoDoc')) ?>
-<?php echo $form['nro_doc']->renderError() ?>
-<?php echo $form['nro_doc']->render(array('class'=>'nroDoc','placeholder'=>'Ingrese su n&uacute;mero de Documento')) ?>
-<?php echo $form['fechanac']->renderLabel("Fecha de Nacimiento:") ?>
-<?php echo $form['fechanac']->renderError() ?>
-<?php echo $form['fechanac'] ?>
-<?php echo $form['sexo']->renderLabel("Sexo:") ?>
-<?php echo $form['sexo']->renderError() ?>
-<?php echo $form['sexo'] ?>
-<?php echo $form['estadocivil']->renderLabel("Estado Civil:") ?>
-<?php echo $form['estadocivil']->renderError() ?>
-<?php echo $form['estadocivil'] ?>
-<?php echo $form['plan_id']->renderLabel("Plan:") ?>
-<?php echo $form['plan_id']->renderError() ?>
-<?php echo $form['plan_id'] ?>
+    <fieldset>
+        <legend>Datos Personales</legend>
+    
+    <div class="campo">
+        <?php echo $form['apenombre']->renderLabel("Nombre y Apellido:") ?>        
+        <?php echo $form['apenombre'] ?>
+        <?php echo $form['apenombre']->renderError() ?>
+    </div>
+    <div class="campo">
+    <label>Documento:</label>
+        <?php echo $form['tipodoc_id']->renderError() ?>
+        <?php echo $form['tipodoc_id']->render(array('class'=>'tipoDoc')) ?>
+        <?php echo $form['nro_doc']->render(array('class'=>'nroDoc','placeholder'=>'Ingrese su n&ordm; de Doc.')) ?>
+        <?php echo $form['nro_doc']->renderError() ?>
+    </div>
+    <div class="campo">
+        <?php echo $form['fechanac']->renderLabel("Fecha de Nacimiento:") ?>
+        <?php echo $form['fechanac']->renderError() ?>
+        <?php echo $form['fechanac'] ?>
+    </div>
+    <div class="campo">
+        <?php echo $form['sexo']->renderLabel("Sexo:") ?>
+        <?php echo $form['sexo']->renderError() ?>
+        <?php echo $form['sexo'] ?>
+    </div>    
+    <div class="campo">
+        <?php echo $form['estadocivil']->renderLabel("Estado Civil:") ?>
+        <?php echo $form['estadocivil']->renderError() ?>
+        <?php echo $form['estadocivil'] ?>
+    </div>    
+    <div class="campo">
+        <?php echo $form['plan_id']->renderLabel("Plan:") ?>
+        <?php echo $form['plan_id']->renderError() ?>
+        <?php echo $form['plan_id'] ?>    
+    </div>
+        </fieldset>
 </div>
 <div id="afiliadoDerecha">
-<?php echo $form['calle']->renderLabel("Domicilio:") ?>
-<?php echo $form['calle']->renderError() ?>
-<?php echo $form['calle']?>
-
-<?php echo $form['altura']->renderLabel("Nro.:") ?>
-<?php echo $form['altura']->renderError();?>
-<?php echo $form['altura']->render(array('class'=>'nroDireccion')) ?>
-
-<?php echo $form['piso']->renderLabel("Piso:") ?>
-<?php echo $form['piso']->renderError() ?>
-<?php echo $form['piso']->render(array('class'=>'nroDireccion')) ?>
-
-<?php echo $form['depto']->renderLabel("Dpto:") ?>
-<?php echo $form['depto']->renderError() ?>
-<?php echo $form['depto']->render(array('class'=>'nroDireccion')) ?>
-
-
-
-<?php echo $form['localidad_id']->renderLabel("Localidad:") ?>
-<?php echo $form['localidad_id']->renderError() ?>
-<?php echo $form['localidad_id'] ?>
-            
-<?php echo $form['reparticion_id']->renderLabel("Repartici&oacute;n:") ?>
-<?php echo $form['reparticion_id']->renderError() ?>
-<?php echo $form['reparticion_id'] ?>
-        
-<?php echo $form['fechaingreso']->renderLabel("Fecha de Ingreso:") ?>
-<?php echo $form['fechaingreso']->renderError() ?>
-<?php echo $form['fechaingreso'] ?>
+    <div class="direccion">
+        <fieldset>
+        <legend>Direcci&oacute;n</legend>
+        <div class="campo">
+        <?php echo $form['calle']->renderLabel("Calle:") ?>
+        <?php echo $form['calle']->renderError() ?>
+        <?php echo $form['calle']?>
+        </div>
+        <div class="num-piso-dpto">
+        <?php //echo $form['altura']->renderLabel("Nro.:") ?>
+            <div class="tit">N&ordm;</div>
+        <?php echo $form['altura']->renderError();?>
+        <?php echo $form['altura']->render(array('class'=>'nroDireccion')) ?>
+            <div class="tit">Piso</div>
+        <?php //echo $form['piso']->renderLabel("Piso:") ?>
+        <?php echo $form['piso']->renderError() ?>
+        <?php echo $form['piso']->render(array('class'=>'nroDireccion')) ?>
+            <div class="tit">Dpto</div>
+        <?php //echo $form['depto']->renderLabel("Dpto:") ?>
+        <?php echo $form['depto']->renderError() ?>
+        <?php echo $form['depto']->render(array('class'=>'nroDireccion')) ?>
+        </div>
+        <div class="campo">
+        <?php echo $form['localidad_id']->renderLabel("Localidad:") ?>
+        <?php echo $form['localidad_id']->renderError() ?>
+        <?php echo $form['localidad_id'] ?>
+        </div>    
+        </fieldset>
+    </div>
+    <fieldset>
+        <legend>Datos Laborales</legend>
+        <div class="campo">
+            <?php echo $form['reparticion_id']->renderLabel("Repartici&oacute;n:") ?>
+            <?php echo $form['reparticion_id']->renderError() ?>
+            <?php echo $form['reparticion_id'] ?>
+        </div>
+        <div class="campo">
+            <?php echo $form['fechaingreso']->renderLabel("Fecha de Ingreso:") ?>
+            <?php echo $form['fechaingreso']->renderError() ?>
+            <?php echo $form['fechaingreso'] ?>    
+        </div>
+        </fieldset>
 </div>
 
 
